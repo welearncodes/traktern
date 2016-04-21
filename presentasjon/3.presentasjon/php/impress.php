de720050895071bf0ff22d69fb2d0dab23b4af61
Kristoffer Risa <kristofferrisa@gmail.com>
2016-04-21 10:18:07 +0200

    3.presentasjon

---
+++ presentasjon/3.presentasjon/php/impress.php
@@ -0,0 +1,15 @@
+<div id="impress">
+      
+<?php include('impress-html/01-intro.html'); ?>
+        
+<?php include('impress-html/02-agenda.html'); ?>
+
+<?php //include('impress-html/03-status.html'); ?>
+
+<?php include('impress-html/04-sikkerhet.html'); ?>
+
+<?php include('impress-html/07-demo.html'); ?>
+
+<?php include('static/hint.html'); ?>
+
+</div>

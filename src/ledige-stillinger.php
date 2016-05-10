7dc9dd50f1c61179b574a784d5ce851ab22f88bd
Kristoffer Risa <kristofferrisa@gmail.com>
2016-05-10 13:15:27 +0200

    masse annet

---
+++ src/ledige-stillinger.php
@@ -0,0 +1,32 @@
+<?php
+
+if ($_POST && !empty($_FILES)) {
+
+    // if the submit button is clicked, send the email
+    if ( isset( $_POST['cf-submitted'] ) ) {
+
+        // sanitize form values
+        $name    = sanitize_text_field( $_POST["cf-name"] );
+        $email   = sanitize_email( $_POST["cf-email"] );
+        $subject = sanitize_text_field( $_POST["cf-subject"] );
+        $message = esc_textarea( $_POST["cf-message"] );
+
+        // get the blog administrator's email address
+        $to = get_option( 'admin_email' );
+
+        $headers = "From: $name <$email>" . "\r\n";
+
+        // If email has been process for sending, display a success message
+        if ( wp_mail( $to, $subject, $message, $headers ) ) {
+            echo '<div>';
+            echo '<p>Thanks for contacting me, expect a response soon.</p>';
+            echo '</div>';
+        } else {
+            echo 'An unexpected error occurred';
+        }
+    }
+
+}
+
+?>
+
